79512d705ee7d6062396e1df1fd1701adf14d7a2
In-Kap Bang <ikpbang@inmoment.com>
2021-07-23 15:48:17 -0400

    remove personal schemas that are irrelevant

--- mssql/oltp/schemas.sql
+++ mssql/oltp/schemas.sql
@@ -24,30 +24,6 @@ if not exists(select s.schema_id from sys.schemas s where s.name = 'rli_con')
 	exec sp_executesql N'create schema [rli_con] authorization [dbo]'
 end
 
-if not exists(select s.schema_id from sys.schemas s where s.name = 'MSHAREX\sql2k8svcact') 
-	and exists(select p.principal_id from sys.database_principals p where p.name = 'MSHAREX\sql2k8svcact') begin
-	exec sp_executesql N'create schema [MSHAREX\sql2k8svcact] authorization [MSHAREX\sql2k8svcact]'
-end
-
-if not exists(select s.schema_id from sys.schemas s where s.name = 'MSHAREX\drobinson') 
-	and exists(select p.principal_id from sys.database_principals p where p.name = 'MSHAREX\drobinson') begin
-	exec sp_executesql N'create schema [MSHAREX\drobinson] authorization [MSHAREX\drobinson]'
-end
-
-if not exists(select s.schema_id from sys.schemas s where s.name = 'MSHAREX\btenney') 
-	and exists(select p.principal_id from sys.database_principals p where p.name = 'MSHAREX\btenney') begin
-	exec sp_executesql N'create schema [MSHAREX\btenney] authorization [MSHAREX\btenney]'
-end
-
-if not exists(select s.schema_id from sys.schemas s where s.name = 'MSHAREX\shart') 
-	and exists(select p.principal_id from sys.database_principals p where p.name = 'MSHAREX\shart') begin
-	exec sp_executesql N'create schema [MSHAREX\shart] authorization [MSHAREX\shart]'
-end
-
-if not exists(select s.schema_id from sys.schemas s where s.name = 'MSHAREX\tpeterson') 
-	and exists(select p.principal_id from sys.database_principals p where p.name = 'MSHAREX\tpeterson') begin
-	exec sp_executesql N'create schema [MSHAREX\tpeterson] authorization [MSHAREX\tpeterson]'
-end
 
 if not exists(select s.schema_id from sys.schemas s where s.name = 'databus') 
 	and exists(select p.principal_id from sys.database_principals p where p.name = 'databus') begin
